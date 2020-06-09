fde0e3bb5b3c0508eb643dfed54ae95960105003
Leonardo Henrique <leonardo.henrique@bistwobis.com.br>
2020-06-09 19:49:56 -0300

    Salvando progresso

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -3,18 +3,29 @@
     <div class="row justify-between shadow-10">
       <q-parallax
         src="../assets/banner/header-home.jpg"
-        class=""
+        style=""
       >
         <div class="text-yellow text-uppercase text-orverline text-weight-medium">Fullstack Web Developer</div>
-        <div class="text-white text-uppercase text-h1 text-weight-medium">Leonardo Henrique</div>
+        <div class="text-white text-uppercase text-h1 text-weight-bolder">Leonardo Henrique</div>
+        <q-btn outline rounded color="yellow" label="About me" class="q-px-xl q-mt-xl" @click="scrollToElement('about-me')" />
       </q-parallax>
     </div>
-    <div style="height: 1000px"></div>
   </q-page>
 </template>
 
 <script>
+import { scroll } from 'quasar'
+const { getScrollTarget, setScrollPosition } = scroll
 export default {
-  name: 'PageIndex'
+  name: 'PageIndex',
+  methods: {
+    scrollToElement (el) {
+      const ele = document.getElementById(el)
+      const target = getScrollTarget(ele)
+      const offset = ele.offsetTop - ele.scrollHeight
+      const duration = 1000
+      setScrollPosition(target, offset, duration)
+    }
+  }
 }
 </script>
