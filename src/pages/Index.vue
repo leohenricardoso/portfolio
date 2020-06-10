712cc72b8b891f17cb38be1119e0a486459f70ee
Leonardo Henrique <leonardo.henrique@bistwobis.com.br>
2020-06-10 00:16:30 -0300

    Salvar progresso

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,15 +1,26 @@
 <template>
   <q-page>
     <div class="row justify-between shadow-10">
-      <q-parallax
-        src="../assets/banner/header-home.jpg"
-        style=""
+      <q-img
+        src="../assets/banner/bg10.jpg"
+        style="height: 100vh"
       >
-        <div class="text-yellow text-uppercase text-orverline text-weight-medium">Fullstack Web Developer</div>
-        <div class="text-white text-uppercase text-h1 text-weight-bolder">Leonardo Henrique</div>
-        <q-btn outline rounded color="yellow" label="About me" class="q-px-xl q-mt-xl" @click="scrollToElement('about-me')" />
-      </q-parallax>
+        <div class="row justify-center items-center full-width full-height text-center">
+            <div class="col-10">
+              <div class="text-uppercase text-overline text-weight-medium">Fullstack Web Developer</div>
+              <div class="text-white text-h1 text-weight-bolder">I'm Leonardo Henrique</div>
+              <q-btn outline rounded label="About me" class="q-px-xl q-mt-xl" @click="scrollToElement('about-me')" />
+            </div>
+        </div>
+      </q-img>
     </div>
+    <q-separator />
+    <div id="about-me" class="row justify-center q-pa-md">
+      <div class="col-12 col-md-6">
+
+      </div>
+    </div>
+    <div style="height:2000px"></div>
   </q-page>
 </template>
 
@@ -23,9 +34,19 @@ export default {
       const ele = document.getElementById(el)
       const target = getScrollTarget(ele)
       const offset = ele.offsetTop - ele.scrollHeight
-      const duration = 1000
+      const duration = 500
       setScrollPosition(target, offset, duration)
     }
   }
 }
 </script>
+
+<style>
+body {
+  /* font-family: 'Josefin Sans', sans-serif; */
+  /* font-family: 'Permanent Marker', cursive; */
+  /* font-family: 'Patrick Hand', cursive; */
+  font-family: 'Sriracha', cursive;
+
+}
+</style>
